beb266187cd4d899ec00aed75f168f0f43d25803
Trần Hoàng Hải ( Bin Dev ) <92655443+Binnndev@users.noreply.github.com>
2025-06-10 03:08:02 +0700

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
-      },
-    ];
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
